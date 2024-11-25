a88a554b7c6b71244fa3ada56925797ded19612f
henriquevro <henriquevro@gmail.com>
2024-11-25 20:46:31 -0300

    Script to count new outputs from block 123,456

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 # How many new outputs were created by block 123,456?
+
+block_hash=$(bitcoin-cli getblockhash 123456)
+block=$(bitcoin-cli getblock "$block_hash")
+txids=$(echo "$block" | jq -r '.tx[]')
+
+new_outputs=0
+
+for txid in $txids; do
+  tx=$(bitcoin-cli getrawtransaction "$txid" 1)
+  outputs=$(echo "$tx" | jq '.vout | length')
+  new_outputs=$((new_outputs + outputs))
+done
+
+echo $new_outputs
