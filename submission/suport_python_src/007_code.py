8d7a7d180b4b1f5662eaf8bb5a9c48cb61880da4
henriquevro <henriquevro@gmail.com>
2025-01-28 20:12:18 -0300

    Correction of the unspent output check in find_unspent_output function

--- submission/suport_python_src/007_code.py
+++ submission/suport_python_src/007_code.py
@@ -9,15 +9,15 @@ def get_block(block_hash):
     result = subprocess.run(['bitcoin-cli', 'getblock', block_hash, '2'], capture_output=True, text=True)
     return json.loads(result.stdout)
 
+def is_unspent(txid, vout_index):
+    result = subprocess.run(['bitcoin-cli', 'gettxout', txid, str(vout_index)], capture_output=True, text=True)
+    return result.stdout.strip() != ''
+
 def find_unspent_output(block):
     for tx in block['tx']:
         for vout in tx['vout']:
-            if 'spentTxId' not in vout:
-                script_pub_key = vout['scriptPubKey']
-                if 'addresses' in script_pub_key:
-                    return script_pub_key['addresses'][0]
-                elif 'address' in script_pub_key:
-                    return script_pub_key['address']
+            if is_unspent(tx['txid'], vout['n']):
+                return vout['scriptPubKey'].get('address')
     return None
 
 block_height = 123321
